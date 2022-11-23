da45b1a048fceddd711b599765e53d898899ceac
Ali Taghawi <marshaltaghawi@gmail.com>
2022-11-24 02:33:23 +0330

    adding second languege "persian" to project; creating 'src/en/' folder and 'src/en/En.js'; moving components folder from 'src/' to './en/components' and render all components in 'En.js'; render 'En.js' in 'App.js' and removing old components;

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Layout from "./components/layout/Layout";
-import LogIn from "./components/LogIn";
-import SignUp from "./components/SignUp";
-
 import { ToastContainer } from "react-toastify";
+import En from "./en/En";
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/*" element={<Navigate to="/login" />} />
-      </Routes>
+    <>
+      <En />
       <ToastContainer />
-    </Layout>
+    </>
   );
 }
 
---
+++ src/en/En.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
+
+
+const En = () => {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Layout>
+  );
+};
+
+export default En;
